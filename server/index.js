c6d6195a5b696708239072026744cbb1790d5a5e
walexz <adegbiteadewaleoluwaseun@gmail.com>
2022-12-10 22:27:05 +0100

    complete the api request for the api/customers route

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const customer = require('../server/routes/customers')
+
+
+
+
+const cors = require('cors');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
+app.use(cors())
+
+
+
+app.use('/api/customers', customer)
+
+
+
+
+
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port,()=> console.log(`listening on port ${port}`));
+
+module.exports = server;
+
