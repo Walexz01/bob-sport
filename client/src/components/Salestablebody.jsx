ff43440d0a0f05d7ceb03775b3dffc703cb44ca0
walexz <adegbiteadewaleoluwaseun@gmail.com>
2023-01-17 14:35:23 +0100

    complete the left side ui for the sales page

---
+++ client/src/components/Salestablebody.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Salestablebody = ({salesList}) => {
+  return (
+    <>
+        <tbody>
+            {
+               salesList.map(({Order_id,Product_Id,Product_Name,Quantity,Unit_Price,Order_Date})=>{
+                return(
+            <tr>
+                <td>{Order_id} </td>
+                <td>{Product_Id} </td>
+                <td>{Product_Name} </td>
+                <td>{Quantity} </td>
+                <td>${Unit_Price} </td>
+                <td>${ Quantity * Unit_Price} </td>
+                <td>{Order_Date} </td>
+            </tr>
+                )
+               }) 
+            }
+        </tbody>  
+    </>
+  )
+}
+
+export default Salestablebody
